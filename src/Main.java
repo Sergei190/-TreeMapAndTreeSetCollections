257d875eb9fcebf73aa7883564aef80088fe9330
Sergei190 <kudrasovcergej@gmail.com>
2023-03-15 10:06:13 +0500

    replaced the object of this remote class with a lambda with the same logic.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +11,12 @@ public class Main {
         people.add(new Person("Fedor", "Dostoevsky", 59));
         people.add(new Person("Leo", "Tolstoy", 82));
         people.add(new Person("Ivan", "Bunin", 83));
+        people.add(new Person("Anna", "Egorova", 17));
+        people.add(new Person("Lera", "Ivanova", 16));
 
-        Collections.sort(people, new ComparatorMaximumNumberOfWordsInASurname(10));
+        Predicate<Person> predicate = (person -> person.getAge() < 18);
+
+        people.removeIf(predicate);
         System.out.println(people);
     }
 }
